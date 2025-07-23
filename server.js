e14f5bb92377ba7c29236468da3ac06cd2033c9d
anand-murthy-r <anu.2004.mys@gmail.com>
2025-07-23 16:09:49 +0530

    fixed redirect

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = new sqlite3.Database('./database/urls.db', (err) => {
 
 db.run(`
     CREATE TABLE IF NOT EXISTS url(
-        ID INTEGER PRIMARY KEY,
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
         LONG_URL TEXT NOT NULL,
         SHORT_URL TEXT,
         EXPIRY TEXT DEFAULT (datetime('now', '+30 minutes'))
@@ -44,7 +44,7 @@ app.post("/shorturls", logger, async (req, res) => {
             if(err) return res.status(500).json({ message: err.message });
         }
     );
-    const new_url = `http://localhost:${PORT}/shorturls/${shortcode}`;
+    const new_url = `http://localhost:${PORT}/${shortcode}`;
     res.status(201).json({ shortLink: new_url, expiry: expiry.toString() });
 
 });
@@ -57,7 +57,34 @@ app.get("/shorturls/:shortcode", (req, res) => {
         (err, row) => {
             if (err) return res.status(500).json({ message: err.message });
             if (!row) return res.status(404).json({ message: "Shortcode not found" });
-            res.json({ short_url: row.SHORT_URL, expiry: row.EXPIRY });
+            // res.redirect(row.LONG_URL);
+            res.json({ short_url: `http://localhost:${PORT}/${row.SHORT_URL}`, expiry: row.EXPIRY });
+        }
+    );
+});
+
+// app.get("/:shortcode", (req, res) => {
+//     const { shortcode } = req.params;
+//     db.get(
+//         "SELECT * FROM url WHERE SHORT_URL = ?",
+//         [shortcode],
+//         (err, row) => {
+//             if(err) return res.status(500).json({message: err.message});
+//             if(!row) return res.status(404).json({message: 'Not LOL'});
+//             res.redirect(row.LONG_URL);
+//         }
+//     );
+// });
+
+app.get("/:shortcode", (req, res) => {
+    const { shortcode } = req.params;
+    console.log("Received shortcode:", shortcode); // 🔍 LOG
+    db.get(
+        "SELECT LONG_URL FROM url WHERE SHORT_URL = ?",
+        [shortcode],
+        (err, row) => {
+            if(!row) res.status(404).json({ message: "Shortcode not found" });
+            res.redirect("https://" + row.LONG_URL);
         }
     );
 });
